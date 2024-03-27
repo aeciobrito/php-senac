2726b86ee004b519c5e95bbb34fc19b416e798e0
aecio@virtualBox <aeciobrito@gmail.com>
2024-03-26 21:14:56 -0300

    05 - Exercícios

---
+++ 05/exercicios.php
@@ -0,0 +1,23 @@
+<?php
+/*
+    Exercício 1 - Acesso a Elementos:
+    Crie um array contendo os dias da semana. Em seguida, exiba o terceiro dia da semana.
+
+    Exercício 2 - Alteração de Elementos:
+    Crie um array contendo os meses do ano. Altere o valor do último elemento para "Dezembro".
+
+    Exercício 3 - Adição de Elementos:
+    Crie um array vazio. Adicione os números de 1 a 5 ao array. Depois, adicione o número 6 ao final do array.
+
+    Exercício 4 - Percorrendo um Array:
+    Crie um array contendo os nomes dos meses do ano. Utilize um loop foreach para exibir todos os meses.
+
+    Exercício 5 - Contagem de Elementos:
+    Crie um array contendo os dias da semana. Conte quantos elementos o array possui e exiba o resultado.
+
+    Exercício 6 - Busca de Elemento:
+    Crie um array contendo os dias da semana. Utilize a função array_search() para encontrar a posição do dia "Sábado" no array e exiba o resultado.
+
+    Exercício 7 - Reversão de Array:
+    Crie um array contendo os números de 1 a 10. Inverta a ordem dos elementos do array e exiba o resultado.
+*/
