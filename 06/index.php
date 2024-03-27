06d192248f60a935a96e6b2f49724c0ae9cf7054
aeciobrito <aeciobrito@gmail.com>
2024-03-27 20:16:36 -0300

    06 - Conversão de Tipos de Dados

--- 06/index.php
+++ 06/index.php
@@ -18,4 +18,37 @@ echo 'Verificação se $string é do tipo string: ' . (is_string($string)? "Sim"
 echo 'Verificação se $booleano é do tipo boolean: ' . (is_bool($booleano)? "Sim" : "Não") . "<br>";
 echo 'Verificação se $array é do tipo array: ' . (is_array($array)? "Sim" : "Não") . "<br>";
 
+// Conversão de Tipos de Dados
+$numero_string = "456";
+$numero_convertido = (int)$numero_string;
+
+echo "Número convertida de string para int: $numero_convertido <br>";
+
+// Demais conversões:
+$numero = 456;
+$string_numero = (string)$numero;
+
+$bool_string = "true";
+$bool_convertido = (bool)$bool_string;
+
+$array_string = "5,2,9";
+$array_convertido = explode(",", $array_string);
+
+echo "Números em uma string antes da conversão : $array_string <br>";
+echo "Array convertido de string para array: ";
+print_r($array_convertido);
+echo "<br>";
+
+# exemplo adicional 1 - conversão para array
+$hora = "20:02:56"; //hora lida de um log
+$array_hora_minuto = explode(":", $hora);
+echo "O evento ocorreu às $array_hora_minuto[0] horas, $array_hora_minuto[1] minutos, $array_hora_minuto[2] segundos.";
+echo "<br>";
+
+# exemplo adicional 2 - conversão para array
+$contatos = "Aecio-Nerildo-JS-Celso-Luana";
+echo "A lista de contatos é: $contatos <br>";
+
+$contatos_array = explode("-", $contatos);
+print_r($contatos_array);
 ?>
